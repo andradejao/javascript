f2649ff31adafe8f49920098795bfe78d2b33852
Joao Andrade <joaoandradeprime99@gmail.com>
2024-05-07 16:31:28 -0300

    Calculo de Média app web

--- exercicios/media/media.js
+++ exercicios/media/media.js
@@ -1,15 +1,32 @@
 let nota1, nota2, nota3, nota4
 
+
 function calcular() {
     nota1 = Number(frmMedia.txtNota1.value.replace(",", "."))
     nota2 = Number(frmMedia.txtNota2.value.replace(",", "."))
     nota3 = Number(frmMedia.txtNota3.value.replace(",", "."))
     nota4 = Number(frmMedia.txtNota4.value.replace(",", "."))
-    if (frmFlex.txtNota1.value === "") {
-        alert("Preencha o valor do etanol")
-        frmFlex.txtEtanol.focus()
-    } else if (frmFlex.txtGasolina.value === "") {
-        alert("Preencha o valor da gasolina")
-        frmFlex.txtGasolina.focus()
-}
+
+    const resultado = (nota1 + nota2 + nota3 + nota4) / 4
+
+    if (frmMedia.txtNota1.value === "") {
+        alert("Preencha o valor da nota")
+    } else if (frmMedia.txtNota2.value === "") {
+        alert("Preencha o valor da nota")
+    } else if (frmMedia.txtNota3.value === "") {
+        alert("Preencha o valor da nota")
+    } else if (frmMedia.txtNota4.value === "") {
+        alert("Preencha o valor da nota")
+    }else {
+        if(resultado < 4 ) {
+            document.getElementById('status').src = "./img/reprovado.jpg"
+            alert(`A média do aluno é ${resultado}, ele está reprovado!`)
+        }else if (resultado >= 7){
+            document.getElementById('status').src = "./img/aprovado.png"
+            alert(`A média do aluno é ${resultado}, ele está aprovado!`)
+        }else {
+            document.getElementById('status').src = "./img/recuperacao.webp"
+            alert(`A média do aluno é ${resultado}, ele está de recuperação!`)
+        }
+    }
 }
