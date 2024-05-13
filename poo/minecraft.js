e67c7dfa71b4b84904148a46286445258fa26d3e
Joao Andrade <joaoandradeprime99@gmail.com>
2024-05-13 16:58:58 -0300

    Continuando o aprendizado de POO

--- poo/minecraft.js
+++ poo/minecraft.js
@@ -28,23 +28,62 @@ class Bloco {
     }
 }
 
+// Herança (Subclasse da classe Modelo)
+class Enxada extends Bloco {
+    // Atributos
+    constructor(textura, resistencia, conquista) {
+        super(textura, resistencia)
+        this.conquista = conquista
+    }
+    // Ações
+    criarEnxada() {
+        console.log("------------------------------------")
+        console.log("-_")
+        console.log(" /")
+        console.log(`Enxada de ${this.textura}`)
+        console.log(`Resistência: ${this.resistencia}`)
+    }
+    arar() {
+        console.log("._._._. Terra Arada!")
+        if (this.conquista === true) {
+            console.log("* Conquista Obtida!")
+        }
+    }
+
+    // Polimorfismo (Sobreescrever o método existente da classe pai)
+    minerar() {
+        console.log("♰ Dano Atribuído!")
+    }
+}
+
 /***** MUNDO *****/
 console.clear()
 console.log(" _____ _____ _____ _____ _____ _____ _____ _____ _____")
 console.log("|     |     |   | |   __|     | __  |  _  |   __|_   _|")
 console.log("| | | |-   -| | | |   __|   --|    -|     |   __| | |")
 console.log("|_|_|_|_____|_|___|_____|_____|__|__|__|__|__|    |_|")
-console.log("")
 
 // Instanciando um objeto
-const bloco1 = new Bloco("Terra",1)
+const bloco1 = new Bloco("Terra", 1)
 bloco1.criarBloco()
 bloco1.construir()
 
-const bloco2 = new Bloco("Madeira",2)
+const bloco2 = new Bloco("Madeira", 2)
 bloco2.criarBloco()
 bloco2.construir()
 
-const bloco3 = new Bloco("Pedra",5)
+const bloco3 = new Bloco("Pedra", 5)
 bloco3.criarBloco()
 bloco3.minerar()
+
+const enxada1 = new Enxada("Madeira", 2, false)
+enxada1.criarEnxada()
+enxada1.arar()
+
+const enxada2 = new Enxada("Ferro", 5, true)
+enxada2.criarEnxada()
+enxada2.arar()
+
+const enxada3 = new Enxada("Diamante", 10, false)
+enxada3.criarEnxada()
+enxada3.minerar()
