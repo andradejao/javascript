8e99859deac311966e4331c429ac47a7e599334d
Joao Andrade <joaoandradeprime99@gmail.com>
2024-05-15 17:14:54 -0300

    PDV exercicio criado

---
+++ exercicios/pdv/pdv.js
@@ -0,0 +1,20 @@
+let totalCompra, desconto, valorPago, valorFinal
+
+function calcularTotal() {
+    totalCompra = Number(frmPdv.txtTotal.value.replace(",", "."))
+    desconto = Number(frmPdv.txtPorcentagem.value.replace(",", "."))
+
+    let totalDesconto = (totalCompra * desconto) / 100
+    valorFinal = totalCompra - totalDesconto
+
+    frmPdv.txtDesconto.value = `${totalDesconto}`
+    frmPdv.txtTotalDesconto.value = `${valorFinal}`
+}
+
+function calcularTroco() {
+    valorPago = Number(frmPdv.txtValorPago.value.replace(",", "."))
+    valorFinal = Number(frmPdv.txtTotalDesconto.value.replace(",", "."))
+    let trocoFinal = valorPago - valorFinal
+
+    frmPdv.txtTroco.value = `${trocoFinal}`
+}
