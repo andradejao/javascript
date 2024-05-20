8934263dcbc17f9144a749eed92d85f1334fd88f
João Vitor Andrade <joaoandradeprime99@gmail.com>
2024-05-20 17:15:20 -0300

    Continuando a desenvolver o exercício de cartas

--- exercicios/cartas/cartas.js
+++ exercicios/cartas/cartas.js
@@ -12,6 +12,25 @@ function sortearCarta() {
     // Sorteio do índice dos vetores
     let nipe = nipes[Math.floor(Math.random() * 4)] // (0 - 3)
     let face = faces[Math.floor(Math.random() * 13)] // (0 - 12)
+    // console.log(`${face}${nipe}`)
 
-    console.log(`${face}${nipe}`)
+
+    // Determinar a cor com base no naipe sorteado antes de exibir a carta
+    let cor
+    if (nipe === '♥' || nipe === '♦') {
+        cor = '#ff0000' // Vermelho
+    } else {
+        cor = '#000000' // Preto
+    }
+
+    // Dar cor as cartas
+    document.getElementById('supEsq').style.color = cor
+    document.getElementById('infDir').style.color = cor
+    document.getElementById('centro').style.color = cor
+
+    // Renderizar(desenhar) a carta
+    document.getElementById('supEsq').innerHTML = `<div>${face}</div><div>${nipe}</div>`
+    document.getElementById('centro').innerHTML = `<div>${nipe}</div>`
+    document.getElementById('infDir').innerHTML = `<div>${face}</div><div>${nipe}</div>`
+
 }
