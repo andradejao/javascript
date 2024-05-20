d11890369ff2464c90299ef6f2aa32bf92b07b2b
João Vitor Andrade <joaoandradeprime99@gmail.com>
2024-05-20 16:05:26 -0300

    Atualizando js

--- exercicios/cartas/cartas.js
+++ exercicios/cartas/cartas.js
@@ -3,6 +3,15 @@
  * @author João Vitor Andrade
  */
 
-function sortearCarta(){
-    alert('teste')
+function sortearCarta() {
+    //           [0]  [1]  [2]  [3]
+    let nipes = ["♦", "♥", "♠", "♣"]
+    //           [0]  [1]  [2]  [3]  [4]  [5]  [6]  [7]  [8]  [9]  [10]  [11] [12]
+    let faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    // Sorteio do índice dos vetores
+    let nipe = nipes[Math.floor(Math.random() * 4)] // (0 - 3)
+    let face = faces[Math.floor(Math.random() * 13)] // (0 - 12)
+
+    console.log(`${face}${nipe}`)
 }
