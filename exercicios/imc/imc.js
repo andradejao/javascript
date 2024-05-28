121efa8b9ece8f5698b6ae3d72c3da1aeb8a08de
João Vitor Andrade <joaoandradeprime99@gmail.com>
2024-05-28 14:11:13 -0300

    App web imc calc criado

--- exercicios/imc/imc.js
+++ exercicios/imc/imc.js
@@ -14,7 +14,25 @@ function calcularImc() {
             document.getElementById('status').src = "./img/underweight.png"
         }
         else if (imc >= 18.5 && imc < 24.9) {
-            document.getElementById('valorImc').innerText = `Seu IMC é: ${imc}`
+            document.getElementById('valorImc').innerText = `Seu IMC é: ${imc.toFixed(1)}. Peso Normal`
             document.getElementById('status').src = "./img/normal.png"
+        } else if (imc >= 25 && imc < 29.9) {
+            document.getElementById('valorImc').innerText = `Seu IMC é: ${imc.toFixed(1)}. Acima do Peso`
+            document.getElementById('status').src = "./img/overweight.png"
+        } else if (imc >= 30 && imc < 34.9) {
+            document.getElementById('valorImc').innerText = `Seu IMC é: ${imc.toFixed(1)}. Obesidade`
+            document.getElementById('status').src = "./img/obese.png"
+        } else {
+            document.getElementById('valorImc').innerText = `Seu IMC é: ${imc.toFixed(1)}. Obesidade Extrema`
+            document.getElementById('status').src = "./img/extremalyobese.png"
         }
+    else {
+        document.getElementById('valorImc').innerText = "O cálculo de IMC não é recomendável para pessoas menores de 18 anos"
+        document.getElementById('status').src = "./img/default.png"
+    }
+}
+
+function limpar() {
+    document.getElementById('valorImc').innerText = ""
+    document.getElementById('status').src = "./img/default.png"
 }
